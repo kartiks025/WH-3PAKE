16d74e4272466b384f0671beb47ce04a75533ccc
Kartik Singhal <kartiks025@gmail.com>
2018-11-17 02:04:58 +0530

    updating gitignore

--- server.cpp
+++ server.cpp
@@ -8,6 +8,7 @@
 #include "helper.hpp"
 #include <vector>
 #define PORTS 8080 
+#define BLOCK_SIZE 16
 
 int main() 
 { 
@@ -36,14 +37,50 @@ int main()
             exit(EXIT_FAILURE); 
         }
         client_sockets.push_back(new_socket);
-        int valread = read( new_socket , buffer, 1024); 
-        printf("%s\n",buffer ); 
-        send(new_socket , hello , strlen(hello) , 0 ); 
-        printf("Hello message sent\n"); 
+        // int valread = read( new_socket , buffer, 1024); 
+        // printf("%s\n",buffer ); 
+        // send(new_socket , hello , strlen(hello) , 0 ); 
+        // printf("Hello message sent\n"); 
     }
     
-    // valread = read( new_socket , buffer, 1024);
-    // printf("%s\n",buffer );
-    // std::cout << M1 << std::endl;
+    unsigned long long G = 19;
+    unsigned long long g = 9; //TODO: QR(19) 4 16 7 9 17 11 6 5 1
+    unsigned long long p = 17;
+    std::string passA = "mukesh_pareek";
+    std::string str_ivA = "hostel9";
+    char* keyA = hash(passA.c_str(), passA.size());
+    char* ivA = hash(str_ivA.c_str(), str_ivA.size());
+    std::string passB = "kartik_singhal";
+    std::string str_ivB = "hostel5";
+    char* keyB = hash(passB.c_str(), passB.size());
+    char* ivB = hash(str_ivB.c_str(), str_ivB.size());
+
+    char M1[BLOCK_SIZE] = {0};
+    char M2[BLOCK_SIZE] = {0};
+    int valread = recv(client_sockets[0] , M1, BLOCK_SIZE, 0);
+    // for(int i=0; i<BLOCK_SIZE ;i++)
+    //   std::cout << (int)M1[i] << std::endl;
+    valread = recv(client_sockets[0] , M2, BLOCK_SIZE, 0);
+    // for(int i=0; i<BLOCK_SIZE ;i++)
+    //   std::cout << (int)M2[i] << std::endl;
+
+    char* gx = decrypt(M1,BLOCK_SIZE,keyA,ivA);
+    unsigned long long g_x = to_long(gx,BLOCK_SIZE);
+    char* gy = decrypt(M2,BLOCK_SIZE,keyB,ivB);
+    unsigned long long g_y = to_long(gy,BLOCK_SIZE);
+    // for(int i=0; i<BLOCK_SIZE ;i++)
+    //   std::cout << (int)gx[i] << " ";
+    // std::cout << std::endl;
+    // for(int i=0; i<BLOCK_SIZE ;i++)
+    //   std::cout << (int)gy[i] << " ";
+    // std::cout << std::endl;
+    std::cout << g_x << std::endl;
+    std::cout << g_y << std::endl;
+    srand(time(NULL));
+    unsigned long long s1 = rand()%p;
+    unsigned long long s2 = rand()%p;
+    unsigned long long kas = ((((long long)pow(g_x,s1))%G+G)%G);
+    unsigned long long kbs = ((((long long)pow(g_x,s2))%G+G)%G);
+
     return 0; 
 } 
