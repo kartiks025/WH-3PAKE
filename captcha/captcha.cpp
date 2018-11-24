47eb3440d312bd7d906a27a072436960d7d55588
Pareek210 <mukeshpareek1997@gmail.com>
2018-11-24 17:30:42 +0530

    Final captcha code

--- captcha/captcha.cpp
+++ captcha/captcha.cpp
@@ -3,22 +3,21 @@
 // #include<conio.h>
 extern "C"
 {
-	void captcha(unsigned char im[70*200], unsigned char l[6]);
-	void makegif(unsigned char im[70*200], unsigned char gif[]);
+	void captcha(unsigned char im[210*200], unsigned char l[16]);
+	void makegif(unsigned char im[210*200], unsigned char gif[]);
 }
 
-int main() {
-	unsigned char l[6] = "abcda";
-	unsigned char im[70*200];
-	unsigned char gif[17646];
+int create_captcha(unsigned char l[16]) {
+	unsigned char im[210*200];
+	unsigned char gif[17646*3];
 
 	captcha(im,l);
 	makegif(im,gif);
 
-	std::cout << gif <<std::endl;
+	// std::cout << gif <<std::endl;
 
-	std::cout.write(reinterpret_cast<const char*>(gif),17646);
-	std::cerr.write(reinterpret_cast<const char*>(l),5);
+	std::cout.write(reinterpret_cast<const char*>(gif),17646*3);
+	std::cerr.write(reinterpret_cast<const char*>(l),15);
 
 	return 0;
 }
